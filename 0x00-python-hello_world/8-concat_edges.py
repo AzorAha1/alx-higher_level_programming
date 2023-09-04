f3a671ad8d6c9384d583ac928a74551d6e343363
AzorAha1 <wizfaiz@icloud.com>
2023-09-04 20:15:59 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-str = str.split(" ")
+str.split(" ")
 print(f"{str[5]} {str[6]} with Python")
