5f40a6381403b42212eaeb9db33aee6c33706d63
AzorAha1 <wizfaiz@icloud.com>
2023-09-04 18:34:38 +0100

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton "
+str1 = "Holberton"
 str2 = "School"
-concat = "Welcome to " + str1 + str2
-print(f'{concat:s}!')
+concat = f"Welcome to {str1} {str2}!"
+print(concat)
