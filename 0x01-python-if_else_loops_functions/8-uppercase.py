eb356cf85d4cef4de3374ace01932585823d4723
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 22:25:30 +0100

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_name = ""
+    for string in str:
+        upstr = ord(string)
+        if 97 <= upstr <= 122:
+            uppercase_name += chr(upstr - 32)
+        else:
+            uppercase_name += string
+    print(uppercase_name)
