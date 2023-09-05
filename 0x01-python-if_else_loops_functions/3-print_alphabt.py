b0a999f3132c45cc320cb1951b115892d197dccd
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 17:38:53 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for string in range(97, 123):
-    alpha = chr(string);
+    alpha = chr(string)
     if alpha != 'q' and alpha != 'e':
         print(alpha.format(alpha), end='')
