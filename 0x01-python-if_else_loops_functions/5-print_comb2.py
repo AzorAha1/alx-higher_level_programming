6cb5346247af15731c6553dab5c1c1b47b8291a1
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 20:04:45 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 99):
     if number == 98:
-        print("{:02}".format(number), end=(''))
+        print(f'{number}', end=(''))
     else:
         print("{:02}".format(number), end=(', '))
 print()
