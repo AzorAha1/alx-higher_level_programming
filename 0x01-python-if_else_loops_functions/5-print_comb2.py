e9f5d77059320340299460872ae894061fefdd91
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 20:11:01 +0100

    requirements error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    if number == 98:
-        print(f'{number}', end=(''))
+    if number == 98: 
+        print("{:02}".format(number))
     else:
-        print("{:02}".format(number), end=(', '))
-print()
+        print("{:02}".format(number), end=', ', flush=True)
