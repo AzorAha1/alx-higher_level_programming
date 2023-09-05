2cdff9f2823d446b94a7fe227e9c920bd3c916fb
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 13:44:16 +0100

    checking if number is negative or not

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,12 @@ number = random.randint(-10000, 10000)
 num_str = repr(number)
 lastdigit = int(num_str[-1])
 string = "Last digit of"
+longstr = "and is less than 6 and not 0"
 if lastdigit < 6 and lastdigit != 0:
     if number < 0:
-        print(f'{string} {number} is -{lastdigit} and is less than 6 and not 0')
+        print(f'{string} {number} is -{lastdigit} {longstr}')
     else:
-        print(f'{string} {number} is {lastdigit} and is less than 6 and not 0')
+        print(f'{string} {number} is {lastdigit} {longstr}')
 elif lastdigit > 5:
     print(f'{string} {number} is {lastdigit} and is greater than 5')
 elif lastdigit == 0:
