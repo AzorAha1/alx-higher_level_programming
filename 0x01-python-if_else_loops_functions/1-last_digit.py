5fdfcefed7bf8fd9f000efcfc5d13da822c0b90d
AzorAha1 <wizfaiz@icloud.com>
2023-09-05 13:56:10 +0100

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,6 @@ longstr = "and is less than 6 and not 0"
 if lastdigit < 6 and lastdigit != 0:
     if number < 0:
         print(f'{string} {number} is -{lastdigit} {longstr}')
-    else:
-        print(f'{string} {number} is {lastdigit} {longstr}')
 elif lastdigit > 5:
     print(f'{string} {number} is {lastdigit} and is greater than 5')
 elif lastdigit == 0:
