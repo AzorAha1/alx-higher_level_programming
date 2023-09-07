8c2a1e430646b3bae5b93fa589109a5e9bb09ae9
AzorAha1 <wizfaiz@icloud.com>
2023-09-07 19:19:54 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-import hidden_4 as hidden
-for i in dir(hidden):
-      if not i.startswith("__"):
-        print("{}".format(i))
+if __name__ == "__main__":
+    import hidden_4 as hidden
+    for i in dir(hidden):
+        if not i.startswith("__"):
+            print("{}".format(i))
