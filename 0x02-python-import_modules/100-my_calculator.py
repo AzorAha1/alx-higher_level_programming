47dcb7849b0a520e84b12588efb14618b482a085
AzorAha1 <wizfaiz@icloud.com>
2023-09-07 23:20:52 +0100

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    args = sys.argv
+    morethan3 = "Unknown operator. Available operators: +, -, * and /"
+
+    if len(args) != 4:
+        print("{}".format(morethan3))
+        exit(1)
+    else:
+        a = args[1]
+        b = args [3]
+        operator = args[2]
+    if operator == "+":
+        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+        exit(0)
+    elif operator == "-":
+        print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+        exit(0)
+    elif operator == "/":
+        print("{} / {} = {}".format(a, b, div(int(a), int(b))))
+        exit(0)
+    elif operator == "*":
+        print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+        exit(0)
+    else:
+        print(morethan3)
+        exit(1)
+    
