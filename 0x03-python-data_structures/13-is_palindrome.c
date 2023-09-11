228c0e4ff6d56b02cda4c6438bdf240da260d17b
AzorAha1 <wizfaiz@icloud.com>
2023-09-11 23:34:03 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,37 +14,4 @@ int is_palindrome(listint_t **head)
 		return (1);
 	}
 
-	listint_t *temp;
-	int list_size;
-
-	temp = *head;
-	while (temp != NULL)
-	{
-		temp = temp->next;
-		list_size++;
-	}
-	int i, j;
-	listint_t *front, *back;
-
-	while (front != NULL && back != NULL)
-	{
-		for (i = 0; i < list_size / 2; i++)
-		{
-			front = *head;
-			back = *head;
-			for (j = 0; j < i; j++)
-			{
-				front = front->next;
-			}
-			for (j = 0; j < list_size - (i + 1); j++)
-			{
-				back = back->next;
-			}
-			if (front->n != back->n)
-			{
-				return (0);
-			}
-		}
-	}
-	return (1);
 }
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return 0, 0
     firstelement = tuple_a[0] + tuple_b[0]
     secondelement = tuple_a[1] + tuple_b[1]
-    return firstelement, secondelement
+    return firstelement + secondelement;
