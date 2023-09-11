c1b8f49c743ef76e2b47085fb048d7307fc7a3e7
AzorAha1 <wizfaiz@icloud.com>
2023-09-12 00:42:47 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        return 0, 0
-    firstelement = tuple_a[0] + tuple_b[0]
-    secondelement = tuple_a[1] + tuple_b[1]
-    return firstelement + secondelement;
+    if len(tuple_a) >= 2:
+        x1, y1 = tuple_a
+    elif len(tuple_a) == 1:
+        x1, y1 = tuple_a[0], 0
+    else:
+        x1, y1 = 0, 0
+    if len(tuple_b) >= 2:
+        x2, y2 = tuple_b
+    elif len(tuple_b) == 1:
+        x2, y2 = tuple_b[0], 0
+    else:
+        x2, y2 = 0, 0
+    output = (x1 + x2, + y1 + y2)
+    return output
