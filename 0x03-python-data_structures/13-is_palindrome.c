9645d0485f7b941bcdb391c7719a04814171661d
AzorAha1 <wizfaiz@icloud.com>
2023-09-11 21:59:09 +0100

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-/**
- * is_palindrome - checks if linked list is a palindrome
- * @head: head 
- * Description - checks for palindrome
- * Return: returns 0 or 1
-*/
+/***
+ * is_palindrome - function name
+ * @head: head pointer
+ * description - function to find if the linked list is a palindrome
+ * Return: return 0 or 1
+ */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL)
-        return 1;
-    listint_t *temp;
-    int list_size;
+	if (*head == NULL)
+	{
+		return (1);
+	}
 
-    temp = *head;
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        list_size++;
-    }
-    int i, j;
-    listint_t *front, *back;
-    while(front != NULL && back != NULL)
-    {
-        for (i = 0; i < list_size / 2; i++)
-        {
-            front = *head;
-            back = *head;
-            for (j = 0; j < i; j++)
-            {
-                front = front->next;
-            }
-            for (j = 0; j < list_size - i; j++)
-            {
-                back = back->next;
-            }
-            if (front->n != back->n)
-            {
-                return (0);
-            }
-        }
-    }
-    return (1);
+	listint_t *temp;
+	int list_size;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		list_size++;
+	}
+	int i, j;
+	listint_t *front, *back;
+
+	while (front != NULL && back != NULL)
+	{
+		for (i = 0; i < list_size / 2; i++)
+		{
+			front = *head;
+			back = *head;
+			for (j = 0; j < i; j++)
+			{
+				front = front->next;
+			}
+			for (j = 0; j < list_size - i; j++)
+			{
+				back = back->next;
+			}
+			if (front->n != back->n)
+			{
+				return (0);
+			}
+		}
+	}
+	return (1);
 }
