d566e0807de71aa46b30b1f81be92ceec217cd3d
AzorAha1 <wizfaiz@icloud.com>
2023-09-13 18:54:14 +0100

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,5 +6,5 @@ def square_matrix_simple(matrix=[]):
         for eachvalue in row:
             squareeachvalue = eachvalue ** 2
             innermat.append(squareeachvalue)
-            output.append(innermat)
-        return output
+        output.append(innermat)
+    return output
