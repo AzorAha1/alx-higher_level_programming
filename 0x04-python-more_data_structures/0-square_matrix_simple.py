8d4ad24c63616ca19ba159f1d3a857797d66132a
AzorAha1 <wizfaiz@icloud.com>
2023-09-13 19:04:20 +0100

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return matrix
     mat = []
     for row in matrix:
         mat.append(list(map(lambda i : i ** 2, row)))
