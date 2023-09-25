b2916e498719f02cd8c0c1ad2792b0240b948ab9
AzorAha1 <wizfaiz@icloud.com>
2023-09-25 22:19:47 +0100

    Read me file added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(mylist=[], x = 0):
-    try:
-        counter = 0
-        if not mylist:
-            return 0
-        for i in mylist:
-            counter+=1
-        if x > counter:
-            x = counter
+    try: 
         [print(i, end='')for i in mylist[:x]]
         print()
         return x
-    except Exception as ex:
-        print("error:", ex)     
+    except:
+        print("error")     
