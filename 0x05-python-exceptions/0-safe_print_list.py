4441beb77394707719401c11bb32d81ed8728d14
AzorAha1 <wizfaiz@icloud.com>
2023-09-25 22:21:33 +0100

    Read me file added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(mylist=[], x = 0):
-    try: 
+    try:
+        counter = 0
+        for c in mylist:
+            counter+=1
+        if x > counter:
+            x = counter
         [print(i, end='')for i in mylist[:x]]
         print()
         return x
