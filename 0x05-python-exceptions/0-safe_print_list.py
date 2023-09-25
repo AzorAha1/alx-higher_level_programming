474d9d4586538ed318f4834f1204229d0988e62c
AzorAha1 <wizfaiz@icloud.com>
2023-09-25 21:59:46 +0100

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(mylist=[], x = 0):
+    if not mylist:
+        return
     try:
         [print(i, end='')for i in mylist[:x]]
         print()
         return x
-
     except:
         print("error")     
