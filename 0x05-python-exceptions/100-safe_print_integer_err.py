ffec91fe9ea45f6e3af12fc564bc99ba8789f286
AzorAha1 <wizfaiz@icloud.com>
2023-09-26 04:35:44 +0100

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        if isinstance(value, int):
+            print("{}".format(value))
+            return True
+        else:
+            print >> sys.stderr(TypeError)
+            return False
+    except Exception as e:
+        print(e)
