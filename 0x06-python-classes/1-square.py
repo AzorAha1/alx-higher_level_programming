546b22521054a6bfbe54134ce49bfed2b8506dfb
AzorAha1 <wizfaiz@icloud.com>
2023-09-26 23:18:03 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class
+this is a class called square
+"""
+
+class Square:
+    """Square class
+    have private attribute called size
+    """
+    ___size = None
+    pass
