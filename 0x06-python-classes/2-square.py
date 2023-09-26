074c25c1e9e86cccbb8685008facb8a00fe75522
AzorAha1 <wizfaiz@icloud.com>
2023-09-27 00:08:52 +0100

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+"""class
+square class
+"""
+
+
+class Square:
+    """Square
+    class
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
