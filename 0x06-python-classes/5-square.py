062b7644b2231ba9191204cb0b41e33bb97c6694
AzorAha1 <wizfaiz@icloud.com>
2023-09-27 03:29:12 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,27 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """class
 comment
 """
 
-
 class Square:
-    """square
-    comment
-    """
-    def __init__(self, __size):
-        self.__size = __size
-
+    def __init__(self, size=0):
+        self.__size = size
     @property
     def size(self):
         return self.__size
-
     @size.setter
     def size(self, value):
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-
-    def area(self):
+        else:
+            self.__size = value
+    def area(self): 
         return self.__size ** 2
+    def my_print(self):
+        if self.size == 0:
+            print("--")
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
