77dde637d4c041503769daa42f69258780835465
AzorAha1 <wizfaiz@icloud.com>
2023-09-27 02:38:26 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,11 +17,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
