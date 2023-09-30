7483fe836b24dae2f5b5ab0213e5cafcf7a31f13
AzorAha1 <wizfaiz@icloud.com>
2023-09-30 23:15:29 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """add
-    >>> add_integer(5, 10)
-    15
-    >>> add_integer(-1, 1)
-    0
-    >>> add_integer(0, 0):
-    0
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100.3, -2)
+    98.3
+    >>> add_integer(2)
+    100
+    >>> add_integer(100, -2)
+    98
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
