fb7e0299bac52c2e4a869de1447e3d45381079fe
AzorAha1 <wizfaiz@icloud.com>
2023-10-01 14:47:44 +0100

    doctest checks for zerodivisionerror

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,9 @@
+import doctest
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     result = [[round(i / div, 2) for i in row] for row in matrix]
     return (result)
+if __name__ == "__main__":
+    doctest.testmod()
