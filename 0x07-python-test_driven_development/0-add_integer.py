b530530602adca76280cc88f611cd2054068b2fb
AzorAha1 <wizfaiz@icloud.com>
2023-10-01 14:30:09 +0100

    removed unneccessary doctest thats already done in 0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """add
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100.3, -2)
-    98.3
-    >>> add_integer(2)
-    100
-    >>> add_integer(100, -2)
-    98
-    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
