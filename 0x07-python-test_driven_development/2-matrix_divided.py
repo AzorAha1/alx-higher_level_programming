c15823ba41143760d63cb37bcbc1b4dfca3f27e1
AzorAha1 <wizfaiz@icloud.com>
2023-10-01 16:17:30 +0100

    3-say_my_name.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,8 @@ import doctest
 def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
     result = [[round(i / div, 2) for i in row] for row in matrix]
     return (result)
 if __name__ == "__main__":
