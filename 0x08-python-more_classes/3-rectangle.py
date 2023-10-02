93e0a9a161b0f417a32c59c9dc88b62857275fe0
AzorAha1 <wizfaiz@icloud.com>
2023-10-03 00:13:58 +0100

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,3 +47,10 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        string_rep = ""
+        for i in range(self.__height):
+            string_rep = string_rep + '#' * self.__width + "\n"
+        return string_rep
