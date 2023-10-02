f59606f47767b8f8c318a0ec0626fb48eb3de7c0
AzorAha1 <wizfaiz@icloud.com>
2023-10-02 17:55:41 +0100

    arrangement and pycodestyle fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,13 +8,14 @@ class Rectangle:
     """Rectangle
     class called Rectangle
     """
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+    def __init__(self, __height, __width):
+        self.__height = __height
+        self.__width = __width
 
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         self.__width = value
@@ -22,9 +23,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         self.__height = value
@@ -32,5 +35,3 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-
-    
