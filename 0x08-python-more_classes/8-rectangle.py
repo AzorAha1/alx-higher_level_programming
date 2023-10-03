10be9cbf8972035b8cf29c79a57a87f4190ec23d
AzorAha1 <wizfaiz@icloud.com>
2023-10-03 04:27:27 +0100

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,9 +85,7 @@ class Rectangle:
         area_rect_1 = rect_1.area()
         area_rect_2 = rect_2.area()
 
-        if area_rect_1 > area_rect_2:
-            return area_rect_1
-        elif area_rect_2 > area_rect_1:
-            return area_rect_2
+        if area_rect_1 >= area_rect_2:
+            return rect_1
         else:
-            return area_rect_1
+            return rect_2
