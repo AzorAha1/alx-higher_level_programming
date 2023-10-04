e69c7189972d13e220d154799c4ab3163adddc6f
AzorAha1 <wizfaiz@icloud.com>
2023-10-04 23:31:42 +0100

    5-text_indentation.

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-def text_indentation(text):
-    for i in range(len(text)):
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print('\n')
-        print(text[i], end="") 
+def text_indentation(text="empty"):
+    in_sentence = False
+    result = ""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for character in text:
+        if character == '.' or character == '?' or character == ':':
+            in_sentence = False
+            result += character + "\n\n"
+        elif character == ' ' and not in_sentence:
+            continue
+        else:
+            result += character
+            in_sentence = True
+    print(result)
