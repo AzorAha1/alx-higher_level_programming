1c0c066aeabb9f2d7abd35e90b017f28bcb0834b
AzorAha1 <wizfaiz@icloud.com>
2023-10-04 15:34:23 +0100

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,5 +13,9 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
+    if not all(isinstance(r, list) and
+               all(isinstance(e, (float, int))for e in r)for r in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     result = [[round(i / div, 2) for i in row] for row in matrix]
     return (result)
