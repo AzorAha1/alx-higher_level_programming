c0721404e6e8ba96c6f1d9d800132edf6d8cc87c
AzorAha1 <wizfaiz@icloud.com>
2023-10-04 12:56:09 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return round(int(a + b))
+    return a + b
 
 
 if __name__ == "__main__":
