63ed28a8823ead7404b8dea025610f12fddcac97
AzorAha1 <wizfaiz@icloud.com>
2023-10-04 14:09:14 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     """add
         this function is to add
     """
-    if (type(a) not in [int, float]) and (a != a or a == float('inf') or a == -float('inf')):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    elif (type(b) not in [int, float]) and (b != b or b == float('inf') or b == -float('inf')):
+    elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
