4fd057b7dae1e75e18f6e01f30c82836e123143a
AzorAha1 <wizfaiz@icloud.com>
2023-10-04 14:22:36 +0100

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,22 @@
-import doctest
 #!/usr/bin/python3
+import doctest
+"""function to divide matrix
+    this divides elements in a matrix
+"""
+
+
 def matrix_divided(matrix, div):
+    """matrix_divided
+        matrix = the matrix
+        div = what we are divided every element with
+    """
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     result = [[round(i / div, 2) for i in row] for row in matrix]
     return (result)
+
+
 if __name__ == "__main__":
     doctest.testmod()
