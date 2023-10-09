f4b2d74f71d19ef979b23399261edb07d090ac9a
AzorAha1 <wizfaiz@icloud.com>
2023-10-09 19:33:36 +0100

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+funtion
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if object is a particular class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
+
