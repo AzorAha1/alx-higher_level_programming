8d6fbb7ecbdc0db13bb00a0e13c20f2fc85fcf7a
AzorAha1 <wizfaiz@icloud.com>
2023-10-09 20:12:13 +0100

    isinstance doesnt check if its an exact type so i have to use type() function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,6 @@ def is_same_class(obj, a_class):
     """
     check if object is a particular class
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
-    elif not isinstance(obj, a_class):
-        return False
+    return False
