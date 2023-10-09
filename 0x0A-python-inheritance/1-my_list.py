b6d830069d3d26ae815cc747cc612465543cf53d
AzorAha1 <wizfaiz@icloud.com>
2023-10-09 19:00:54 +0100

    1-my_list.py with doctest

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class my list
+"""
+
+
+class MyList(list):
+    """Mylist
+    """
+    def print_sorted(self):
+        """
+        print list in sorted
+        """
+        print(sorted(self))
