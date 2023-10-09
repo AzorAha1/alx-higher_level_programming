99f6937957926d78beaed8e3bb40f6f44b7f9571
AzorAha1 <wizfaiz@icloud.com>
2023-10-09 23:48:32 +0100

    only check if it inherited from a class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,8 @@ def inherits_from(obj, a_class):
     """
     check if obj is an instance of a class that inherits from specified class
     """
+    if type(obj) == a_class:
+        return False
     if issubclass(type(obj), a_class):
         return True
     return False
