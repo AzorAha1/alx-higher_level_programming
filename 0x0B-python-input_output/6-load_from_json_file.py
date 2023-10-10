930309893658ea006de7d43eac6ef61f91ef1d03
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 22:55:35 +0100

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""function
+"""
 import json
+
+
 def load_from_json_file(filename):
+    """load_from_json_file
+    """
     with open(file=filename, mode='r') as thefile:
 
         return json.loads(thefile.read())
