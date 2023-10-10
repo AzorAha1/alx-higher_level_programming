9b6af0cdd963f0b2fcd4d47f5e8b1091f9b57ada
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 04:48:39 +0100

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,3 +6,12 @@
 class MyInt(int):
     """MyInt
     """
+    def __eq__(self, value):
+        if not isinstance(value, int):
+            return super().__eq__(value)
+        return False
+
+    def __ne__(self, value):
+        if not isinstance(value, int):
+            return super().__ne__(value)
+        return True
