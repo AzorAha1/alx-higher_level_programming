ed5fd94ac1f4976b93ac048c05c69526d3944555
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 04:08:47 +0100

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """working on inheritance
 """
+Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class BaseGeometry:
@@ -15,23 +16,6 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle
-    inherits from BaseGeometry
-    """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        super().integer_validator(name="width", value=self.__width)
-        super().integer_validator(name="height", value=self.__height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
 
 
 class Square(Rectangle):
