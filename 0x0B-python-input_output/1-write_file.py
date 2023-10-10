2356295a472553a92071c8a4fdc3e5edb72f9ceb
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 18:23:56 +0100

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,9 @@
 
 
 def write_file(filename="", text=""):
+    """write_file
+    writes to a file and returns count
+    """
     with open(filename, 'w', encoding="utf-8") as thefile:
         thefile.write(text)
         return thefile.tell()
