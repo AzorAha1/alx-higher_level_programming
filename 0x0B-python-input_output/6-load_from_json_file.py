23a0e709049c4668401a7efe06cd823580447705
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 22:56:45 +0100

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ def load_from_json_file(filename):
     """load_from_json_file
     """
     with open(file=filename, mode='r') as thefile:
-
-        return json.loads(thefile.read())
+        theobj = json.loads(thefile.read())
+        return theobj
