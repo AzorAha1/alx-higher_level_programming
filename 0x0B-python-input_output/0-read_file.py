35d56e46c8e16c25867ed81af15c13eefb161584
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 18:05:44 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 
 def read_file(filename=""):
     """read_file
-    reads from filename
+    reads from filenamef
     """
     with open(filename, 'r', encoding="utf-8") as thefile:
-        print(thefile.read())
+        print(thefile.read(), end='')
