d8d9d2f0e005d30eb4a575d4a865cc63034e77cc
AzorAha1 <wizfaiz@icloud.com>
2023-10-10 19:59:39 +0100

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,6 +8,5 @@ def append_write(filename="", text=""):
     append to a file and returns count
     """
     with open(filename, 'a', encoding="utf-8") as thefile:
-        thefile.seek(0)
         thefile.write(text)
         return thefile.tell()
