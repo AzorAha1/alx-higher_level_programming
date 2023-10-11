e3ea42a5e4ad973a682af011af47ec64a877f1c2
AzorAha1 <wizfaiz@icloud.com>
2023-10-11 02:40:27 +0100

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """function
 """
-json = __import__('json')
 
 
 def class_to_json(obj):
     """class_to_json
     """
-    jsoninstr = json.dumps(obj=obj.__dict__)
-    return json.loads((jsoninstr))
+    return obj.__dict__
