8fc027ae2d3871049e8c86a700c0e84b51a93394
AzorAha1 <wizfaiz@icloud.com>
2023-10-13 23:17:39 +0100

    so many changes to improve code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,40 +5,106 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        if self.__height <= 0:
+            raise ValueError("width must be > 0")
+        if not isinstance(self.__x, int):
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+        if not isinstance(self.__y, int):
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
+
         super().__init__(id=id)
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
         return self.__y
+
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def display(self):
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+    
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+import unittest
+from models.rectangle import Rectangle
+
+class TestRec(unittest.TestCase):
+    def test_rectangle(self):
+            r1 = Rectangle(10, 12)
+            r2 = Rectangle(2, 10)
+            r3 = Rectangle(10, 2, 0, 0, 12)
+            self.assertEqual(r1.id, 1)
+            self.assertEqual(r2.id, 2)
+            self.assertEqual(r3.id, 12)
