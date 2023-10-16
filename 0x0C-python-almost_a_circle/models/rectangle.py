40eb431a010b64611561782251535e3fe81276b7
AzorAha1 <wizfaiz@icloud.com>
2023-10-16 19:46:39 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,7 +106,8 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}){self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return (f"[Rectangle]({self.id}){self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
         if args:
