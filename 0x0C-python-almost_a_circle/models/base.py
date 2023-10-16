d1179c36633dd330adfd20e660931c65e4027cc8
AzorAha1 <wizfaiz@icloud.com>
2023-10-17 00:34:59 +0100

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,10 +32,13 @@ class Base:
         listobj
         """
         list_objs_list = []
-        with open(file="{}.json".format(cls.__name__), mode='w') as fp:
-            for obj in list_objs:
-                list_objs_list.append(obj.to_dictionary())
-            fp.write(cls.to_json_string(list_dictionaries=list_objs_list))
+        if list_objs is None:
+            return list_objs_list
+        else:
+            with open(file="{}.json".format(cls.__name__), mode='w') as fp:
+                for obj in list_objs:
+                    list_objs_list.append(obj.to_dictionary())
+                fp.write(cls.to_json_string(list_dictionaries=list_objs_list))
 
     def from_json_string(json_string):
         """from json string"""
