23af084e22c58789c0ca14402a0340a1746aaee2
AzorAha1 <wizfaiz@icloud.com>
2023-11-15 05:54:47 +0100

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,10 @@
+SET NAMES utf8mb4
+
 -- convert database to utf8
 ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- convert table
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- convert field
 ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-SHOW CREATE TABLE hbtn_0c_0;
