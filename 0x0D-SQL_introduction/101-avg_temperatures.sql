c316c465b2fe040cadfc6e4bc9fb4fc9ace23f15
AzorAha1 <wizfaiz@icloud.com>
2023-11-15 05:40:06 +0100

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,4 @@
--- avg temperature
+-- import temperature.sql
 mysql -u root -p -D hbtn_0c_0 < temperatures.sql
+-- this does the logic
 SELECT city, AVG(temperatures) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC
