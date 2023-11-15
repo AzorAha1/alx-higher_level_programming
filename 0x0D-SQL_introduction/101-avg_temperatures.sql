5012a66bb57cdf7a70a4fa266d1465c936cc248e
AzorAha1 <wizfaiz@icloud.com>
2023-11-15 05:38:12 +0100

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- avg temperature
-mysql -u root -p -D hbtn_0c_0 < temperatures.sql;
+mysql -u root -p -D hbtn_0c_0 < temperatures.sql
 SELECT city, AVG(temperatures) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC; 
