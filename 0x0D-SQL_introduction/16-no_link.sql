69eaaed5f17ebdf303f00a66860f8de71adaa04c
AzorAha1 <wizfaiz@icloud.com>
2023-11-15 03:23:08 +0100

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- list all records without with name value
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
