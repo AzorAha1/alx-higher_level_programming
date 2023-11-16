06af7daea5c41992e489c8711da557945ab960b1
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 02:11:45 +0100

    name column added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use hbtn_0d_usa database
 USE hbtn_0d_usa;
 -- create table with stateid being a foreign key that references the id of the states table
-CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR NOT NULL);
+CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
