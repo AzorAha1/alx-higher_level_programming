6ce86fbb00a260a6c235a93d5904afe04db16171
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 02:09:11 +0100

    7-citites.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use hbtn_0d_usa database
-USE hbtn_0d_usa;
+-- USE hbtn_0d_usa;
 -- create table with stateid being a foreign key that references the id of the states table
 CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
