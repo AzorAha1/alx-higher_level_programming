a425e2dd7f9edec1c54214329145d43aefcbf2ee
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 02:24:52 +0100

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- subqueries
-SELECT cities IN hbtn_0d_2 WHERE (SELECT * WHERE name = 'California' ORDER BY cities.id ASC)
+SELECT cities IN hbtn_0d_usa WHERE (SELECT * WHERE name = 'California' ORDER BY cities.id ASC)
