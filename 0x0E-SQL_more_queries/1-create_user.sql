588b4c74bbb37ea4b0bcb5594f26a4651341b252
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 01:07:49 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- give priviledges
+GRANT * on *.* to 'user_0d_1'@'localhost';
 -- show grants
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
