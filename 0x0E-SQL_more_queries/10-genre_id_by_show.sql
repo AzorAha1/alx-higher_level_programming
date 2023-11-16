b467abd26b48054e5c51123a56653d59208bf17e
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 03:42:41 +0100

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- genre
 curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql" -s | mysql -uroot -p hbtn_0d_tvshows;
-SELECT tv_shows.title, tv_shows_genre.genre_id FROM tv_shows JOIN tv_shows_genre ON tv_shows.id = tv_shows_genre.id;
+SELECT tv_shows.title, tv_shows_genre.genre_id FROM tv_shows JOIN tv_shows_genre ON tv_shows.id = tv_shows_genre.genre_id ORDER BY tv_shows.title, tv_shows_genre.genre_id;
