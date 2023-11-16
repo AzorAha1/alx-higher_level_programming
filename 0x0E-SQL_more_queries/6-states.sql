5cc09ef6010d4401ae2983f6b2b298cbd6c0b2a0
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 01:49:43 +0100

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table
-USE DATABASE
+USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS states(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
