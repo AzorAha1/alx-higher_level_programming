6930705e855f5a02528c926250d87bcd3a5818c9
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 01:15:51 +0100

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE database IF NOT EXISTS hbtn_0d_2;
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant select priviledge
-GRANT SELECT ON database.* to 'user_0d_2'@'localhost';
+GRANT SELECT ON database.* TO 'user_0d_2'@'localhost';
