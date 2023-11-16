e9915d0f950fd7eabd97cb35d7ff52cffd6a209d
AzorAha1 <wizfaiz@icloud.com>
2023-11-16 10:01:49 +0100

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,4 @@
 -- genre
-curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql" -s | mysql -uroot -p hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
