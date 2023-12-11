774b7a5464688ec9e65a33d91369fef2909284bf
AzorAha1 <wizfaiz@icloud.com>
2023-12-12 00:08:43 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+function add (a, b) {
+  return parseInt(a, 10) + parseInt(b, 10);
+}
+if (args.length > 2) {
+  const result = add(args[2], args[3]);
+  console.log(result);
+}
