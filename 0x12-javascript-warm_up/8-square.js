313c337074349031035c7fe6c99d446da80888c1
AzorAha1 <wizfaiz@icloud.com>
2023-12-11 23:53:21 +0100

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length > 2) {
+  const number = parseInt(args[2], 10);
+  for (let i = 0; i < number; i++) {
+    let row = '';
+    for (let j = 0; j < number; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+  if (isNaN(number)) {
+    console.log('Missing Size');
+  }
+} else {
+  console.log('Missing Size');
+}
