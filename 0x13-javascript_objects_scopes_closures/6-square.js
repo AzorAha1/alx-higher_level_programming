e40bb5c2b567abdf77db0902a8bba9fd87c24fcb
AzorAha1 <wizfaiz@icloud.com>
2023-12-13 00:12:22 +0100

    6-

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        if (c != undefined) {
+          row += 'C';
+        } else if (c === undefined) {
+          row += 'X';
+        }
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
