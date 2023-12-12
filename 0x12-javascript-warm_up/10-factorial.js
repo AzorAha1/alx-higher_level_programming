6ae8bd95c52ffec8b7ce6dbc92adc89e60921fe2
AzorAha1 <wizfaiz@icloud.com>
2023-12-12 01:11:23 +0100

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+const n = args[2];
+function factorial (n) {
+  if (n === 1 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(n));
