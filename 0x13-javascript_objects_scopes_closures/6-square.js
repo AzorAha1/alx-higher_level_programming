c15e675d2da5b3a674fcbbe708a46e64e2732fcb
AzorAha1 <wizfaiz@icloud.com>
2023-12-13 01:40:26 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,9 +8,9 @@ class Square extends LastSquare {
   }
 
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.width; i++) {
       let row = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         if (c !== undefined) {
           row += c;
         } else {
