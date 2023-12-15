447d94686e7c348143d479a405d167d44b9a8d46
AzorAha1 <wizfaiz@icloud.com>
2023-12-15 15:55:20 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,20 +9,24 @@ def list_states_filter():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    database = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        password=password,
-        database=database, port=3306
-        )
-    query = f"select * from states where name like 'N%' order by states.id ASC"
-    cursor = database.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    database.close()
+    try:
+        database = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            password=password,
+            database=database, port=3306
+            )
+        query = f'select * from states where name like "N%" order by states.id ASC'
+        cursor = database.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as e:
+        print(f'the error was {e}')
+    finally:
+        cursor.close()
+        database.close()
 
 
 if __name__ == "__main__":
