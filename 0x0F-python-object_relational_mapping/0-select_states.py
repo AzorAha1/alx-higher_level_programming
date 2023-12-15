a2a84eb208f2161cac9fb022311dc5de2e913a9e
AzorAha1 <wizfaiz@icloud.com>
2023-12-15 04:46:28 +0100

    0-select_state

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+database = MySQLdb.connect(host='localhost', user=username, password=password, database=database, port=3306);
+query = f'select * from states order by states.id ASC'
+cursor = database.cursor()
+cursor.execute(query)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+database.close()
