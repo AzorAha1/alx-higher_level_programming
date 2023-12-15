b50e60a62549b632efeaca3124915f007d68b0a5
AzorAha1 <wizfaiz@icloud.com>
2023-12-15 17:31:28 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,8 @@ def list_states_filter():
         password=password,
         database=database, port=3306
         )
-    q = f'select * from states where name like "N%" COLLATE utf8mb4_bin order by states.id ASC'
+    q = f'select * from states
+    where name like "N%" COLLATE utf8mb4_bin order by states.id ASC'
     cursor = database.cursor()
     cursor.execute(q)
     rows = cursor.fetchall()
