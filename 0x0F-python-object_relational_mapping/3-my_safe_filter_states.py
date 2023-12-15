cde5d1fc9511a133daeb6415e8444a21bfe550be
AzorAha1 <wizfaiz@icloud.com>
2023-12-15 20:43:31 +0100

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""list all states from database(hbtn_0e_0_usa)"""
+
+
+def list_states_filter_safe():
+    """function to list all states with filter"""
+    import sys
+    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sn = sys.argv[4]
+    database = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        password=password,
+        database=database, port=3306
+        )
+    q = 'select * from states ' \
+        'where name=%s COLLATE utf8mb4_bin order by states.id ASC'
+    cursor = database.cursor()
+    cursor.execute(q, (sn,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    list_states_filter_safe()
