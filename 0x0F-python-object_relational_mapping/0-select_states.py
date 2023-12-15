f1c3f6f4a2fd2f072c41fec9a6114a9a126cff8c
AzorAha1 <wizfaiz@icloud.com>
2023-12-15 04:55:39 +0100

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """list all states from database(hbtn_0e_0_usa)"""
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
