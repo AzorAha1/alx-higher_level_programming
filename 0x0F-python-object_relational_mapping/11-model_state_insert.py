5bee19693e605c23c8b9f451cf3cdbd50a83698b
AzorAha1 <wizfaiz@icloud.com>
2023-12-16 04:57:39 +0100

    11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -2,8 +2,8 @@
 """this is a function"""
 
 
-def list_state_obj_with_name():
-    """list state obj with name"""
+def add_new_state_obj():
+    """add new state obj"""
     import sys
     from sqlalchemy import create_engine
     from model_state import Base, State
@@ -11,20 +11,16 @@ def list_state_obj_with_name():
     username = sys.argv[1]
     password = sys.argv[2]
     databasename = sys.argv[3]
-    name = sys.argv[4]
     url = f'mysql://{username}:{password}@localhost:3306/{databasename}'
     engine = create_engine(url)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    filter_name = session.query(State) \
-        .filter(State.name == name).order_by(State.id)
-    if filter_name:
-        for fn in filter_name:
-            print(fn.id)
-    else:
-        print('Not found')
+    newobj = State(name='Louisiana')
+    session.add(newobj)
+    session.commit()
+    print(newobj.id)
 
 
 if __name__ == "__main__":
-    list_state_obj_with_name()
+    add_new_state_obj()
