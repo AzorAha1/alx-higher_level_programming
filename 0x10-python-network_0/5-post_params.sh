67caaaaecc1f8300f9d877482f8281e494d23aa9
AzorAha1 <wizfaiz@icloud.com>
2024-01-06 04:01:45 +0100

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a post request to url and display the body respounse with variables
-curl -sL -X POST $1 -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
+curl -sL -X POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
