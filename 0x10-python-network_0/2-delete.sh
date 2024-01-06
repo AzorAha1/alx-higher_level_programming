cd364b18630a1d180fa8c578a49363e3e4a2edc8
AzorAha1 <wizfaiz@icloud.com>
2024-01-06 02:59:44 +0100

    3-methods.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a DELETE request to the url passed and displays the body of response
-curl -sI "$1" | grep -i "allow" | awk '{print $2}'
+curl -sL -X DELETE $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes a url and shows the http methods the server will accept
-curl -s
+curl -sI $1 | grep -i 'allow' | awk '{print $2}'
