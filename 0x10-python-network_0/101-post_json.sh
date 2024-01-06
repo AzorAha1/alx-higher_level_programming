3bcd1120c77b413f6d3bf661a1fbc0153da75ba6
AzorAha1 <wizfaiz@icloud.com>
2024-01-06 05:32:10 +0100

    100-status_code.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request to url and display body of the response
-curl -s -X POST -H "Content-Type: application/json" -d '{"key1": "value1", "key2":"value2"}' $1
+curl -s -X POST -H "Content-Type: application/json" -d '{"key": "value"}' $1
