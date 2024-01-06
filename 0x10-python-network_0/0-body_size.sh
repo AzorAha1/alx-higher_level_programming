ca11d98559fe998ffd7100d2fb28adb6438d0c14
AzorAha1 <wizfaiz@icloud.com>
2024-01-06 02:22:17 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #bash sccript that takes url send request and display size of the response
-curl -sI 'http://www.example.com' | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
