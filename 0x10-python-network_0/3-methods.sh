1a4f599a34320167391d009a06e78b1237b48524
AzorAha1 <wizfaiz@icloud.com>
2024-01-06 03:27:26 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes a url and shows the http methods the server will accept
-curl -sI "$1" | grep -i "Allow" | awk '{for (i=2; i<=NF; i++) {print "%s", $i}}'
+curl -sI "$1" | grep -i "Allow" | awk '{for (i=2; i<=NF; i++) {printf "%s ", $i}}'
