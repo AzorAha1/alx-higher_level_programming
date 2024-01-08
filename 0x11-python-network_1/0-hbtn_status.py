deeef854902339e328d1173ef4b269e26724a5b2
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 01:43:08 +0100

    update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# this will fetch a certain site
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    print(response.read)
