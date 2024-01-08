43e7a048d532385dd2f5c77c8302e33fc5fce8bb
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 03:45:04 +0100

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,11 @@
 if __name__ == "__main__":
     import urllib.request
     import sys
+    import urllib.parse
     url = sys.argv[1]
     email = sys.argv[2]
-    with urllib.request.urlopen(url) as response:
-        print(dir(response))
+    
+    paramset = urllib.parse.urlencode({'email':email}).encode('utf-8')
+    request_type = urllib.request.Request(url=url, data=paramset,method='POST')
+    with urllib.request.urlopen(request_type) as response:
+        print(response.read())
