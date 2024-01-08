3853a91ed5587ae5636746ad06e3ec516e543358
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 06:12:10 +0100

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,7 +2,9 @@
 """take url send request and display body with status code & error handling"""
 if __name__ == "__main__":
     import requests
-    import sys        
+    import sys
+    if len(sys.argv) < 2:
+        letter = ""   
     letter = sys.argv[1]
     if letter:
         data = {'q':letter}
