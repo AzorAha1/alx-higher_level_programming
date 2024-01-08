04066835f7034d90b57ef71f1c18d3665afb4a4c
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 05:15:07 +0100

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""take url and email ans set a post request with email as param"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    with requests.post(url=url, data=data) as response:
+        print(response)
