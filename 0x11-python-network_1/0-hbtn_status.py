da688ad0d82907642d2da153a53a31fd2a4bc1ac
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 02:12:08 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,5 +3,14 @@
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
-    body_utf8 = response.decode('utf-8')
-    print(f"- type: {type(body_utf8)}")
+    body_utf8 = body.decode('utf-8')
+    type = f"- type: {type(body)}"
+    content = f"- content: {body}"
+    utf8_content = f"- utf8 content: {body}"
+    print(f'''
+    Body response:
+        {type}
+        {content}
+        {utf8_content}
+    ''' 
+    )
