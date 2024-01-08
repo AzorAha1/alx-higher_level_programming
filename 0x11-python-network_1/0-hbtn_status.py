cd501ff232893920e1d950e4a928849d0b072b49
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 02:45:19 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status using urllib"""
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    body_utf8 = body.decode('utf-8')
-
-    print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body}")
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        body_utf8 = body.decode('utf-8')
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body}")
 
