68f3b25d40b1674b580ad39bed8d904a9d7e9566
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 04:11:39 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as error:
-        print(f'Error code: {error.code}')
+        print(f'Error code: {response.status_code}')
