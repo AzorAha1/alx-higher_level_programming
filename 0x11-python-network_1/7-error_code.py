99188bee35e3ad8c64e08c5794372b37c7b2d1ec
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 05:27:59 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""take url send request and display body with status code & error handling"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    try:
+        with requests.get(url) as response:
+            print(response.text)
+    except requests.HTTPError as errror:
+        if requests.status_codes >= 400:
+            print(f'Error code: {errror.errno}')
