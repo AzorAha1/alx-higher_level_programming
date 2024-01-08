445303b9f107529a68cbac456a9929553fd7ae51
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 05:34:21 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with requests.get(url) as response:
-            #response.raise_for_status()
+            response.raise_for_status()
             print(response.text)
     except requests.HTTPError as errror:
         print(f'Error code: {errror.response.status_code}')
