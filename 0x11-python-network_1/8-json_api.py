43949ab41670822af00bab561225e75115f5616b
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 05:55:05 +0100

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""take url send request and display body with status code & error handling"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    letter = sys.argv[1]
+    data = {'q':letter}
+    if not letter:
+        data = {'q':""}
+    response = requests.post(data=data, url='http://0.0.0.0:5000/search_user')
+    try:
+        json_code = response.json();
+    except ValueError:
+        print("Not a valid JSON")
+    if json_code == {}:
+        print('No result')
+    else:
+        print(f"[{response.get('id')}] {response.get('name')}")
+    
+    
+    
