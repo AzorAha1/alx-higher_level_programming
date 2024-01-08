1409f649e1a3a7201a5b26bb3aefd96265e1553e
AzorAha1 <wizfaiz@icloud.com>
2024-01-08 02:37:33 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""fetches https:alx-intranet.hbtn.io/status using urllib"""
 import urllib.request
-# this will fetch a certain site
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
     body_utf8 = body.decode('utf-8')
+
     type = f"- type: {type(body)}"
     content = f"- content: {body}"
     utf8_content = f"- utf8 content: {body}"
