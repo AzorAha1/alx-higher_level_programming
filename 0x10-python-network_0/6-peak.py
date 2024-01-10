512cf5c37f8579f901dbc6d462e2e0e1b949f95f
AzorAha1 <wizfaiz@icloud.com>
2024-01-10 07:58:30 +0100

    6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function to find peak"""
 def find_peak(list_of_integers):
+    """function to find peak in a list of integers"""
     lenoflist = len(list_of_integers)
     nums = list_of_integers
     for i in range(lenoflist):
