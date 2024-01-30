5ea02c0f52c58e3efab49753c89bf50f2625fbe8
AzorAha1 <wizfaiz@icloud.com>
2024-01-31 00:35:36 +0100

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const argv = process.argv;
-url = argv[2]
+const url = argv[2];
 request(url, (error, response, body) => {
-    if (error){
-        console.log(error)
-    }else{
-        console.log('code: ', response.statusCode)
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
 });
