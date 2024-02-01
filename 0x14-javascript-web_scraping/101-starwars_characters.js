07df10c8ac2704ba53decef43196baa2f64976e2
AzorAha1 <wizfaiz@icloud.com>
2024-02-01 03:32:40 +0100

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,15 +8,22 @@ request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response,
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    data.characters.forEach(element => {
-      request.get(element, (error, response, body) => {
+    let index = 0;
+    function move () {
+      if (index === data.characters.length) {
+        return;
+      }
+      request.get(data.characters[index], (error, response, body) => {
         if (error) {
           console.log(error);
         } else {
           const thedata = JSON.parse(body);
           console.log(thedata.name);
+          index++;
+          move();
         }
       });
-    });
+    }
+    move();
   }
 });
