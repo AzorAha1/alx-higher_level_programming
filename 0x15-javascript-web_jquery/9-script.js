b64998181ef7692bb772a3b5b565a47d87dd3cc6
AzorAha1 <wizfaiz@icloud.com>
2024-02-08 05:49:41 +0100

    9-main.html

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.get("https://hellosalut.stefanbohacek.dev/?lang=fr",
+        function (data, textStatus, jqXHR) {
+            $('DIV#hello').text(data.hello);
+        },
+    );
+});
